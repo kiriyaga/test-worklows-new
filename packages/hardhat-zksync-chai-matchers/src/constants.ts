be074364e9acff91cde2d40d9d6193dbbca9dde8
Marko Arambasic <makiarambasic@gmail.com>
2023-12-21 17:37:07 +0100

    fix: delete new constant

--- packages/hardhat-zksync-chai-matchers/src/constants.ts
+++ packages/hardhat-zksync-chai-matchers/src/constants.ts
@@ -1,3 +1,2 @@
 export const PLUGIN_NAME = '@matterlabs/hardhat-zksync-chai-matchers';
 export const PLUGIN_NAME_V5 = '@matterlabs/hardhat-zksync-chai-matchers';
-export const PLUGIN_NAME_V6 = '@matterlabs/hardhat-zksync-chai-matchers';
