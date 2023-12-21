b9faff5eea9d49e22bf813b0448e76c73ca1b55d
Marko Arambasic <makiarambasic@gmail.com>
2023-12-21 15:47:36 +0100

    chore(hardhat-zksync-chai-matchers): new version of the plugin

--- packages/hardhat-zksync-chai-matchers/src/constants.ts
+++ packages/hardhat-zksync-chai-matchers/src/constants.ts
@@ -1,2 +1,2 @@
 export const PLUGIN_NAME = '@matterlabs/hardhat-zksync-chai-matchers';
-export const PLUGIN_NAME_V4 = '@matterlabs/hardhat-zksync-chai-matchers';
+export const PLUGIN_NAME_V5 = '@matterlabs/hardhat-zksync-chai-matchers';
