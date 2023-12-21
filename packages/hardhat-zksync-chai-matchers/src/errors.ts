2abcb85fd4bb412a443e657958baa7aa1a60df3c
Marko Arambasic <makiarambasic@gmail.com>
2023-12-21 16:45:34 +0100

    fix: changed error message to decodding

--- packages/hardhat-zksync-chai-matchers/src/errors.ts
+++ packages/hardhat-zksync-chai-matchers/src/errors.ts
@@ -4,7 +4,7 @@ import { PLUGIN_NAME } from './constants';
 
 export class ZkSyncChaiMatchersPluginError extends HardhatPluginError {
     constructor(encodedData: string, type: string, parentError?: Error) {
-        const message = `There was an error decoding '${encodedData}' as a ${type}`;
+        const message = `There was an error decodingg '${encodedData}' as a ${type}`;
 
         super(PLUGIN_NAME, message, parentError);
     }
