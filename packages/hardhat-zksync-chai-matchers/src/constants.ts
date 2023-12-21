adc4911dbf1c54087fdcedc36460d54d88b4755f
Marko Arambasic <makiarambasic@gmail.com>
2023-12-21 17:25:23 +0100

    fix: added new constant

--- packages/hardhat-zksync-chai-matchers/src/constants.ts
+++ packages/hardhat-zksync-chai-matchers/src/constants.ts
@@ -1,2 +1,3 @@
 export const PLUGIN_NAME = '@matterlabs/hardhat-zksync-chai-matchers';
 export const PLUGIN_NAME_V5 = '@matterlabs/hardhat-zksync-chai-matchers';
+export const PLUGIN_NAME_V6 = '@matterlabs/hardhat-zksync-chai-matchers';
