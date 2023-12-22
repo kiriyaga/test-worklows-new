91644994af67e8950ea506b6f471f0e819dd7e8a
Marko Arambasic <38253493+kiriyaga@users.noreply.github.com>
2023-12-22 11:47:55 +0100

    feat: new feature (#24)

--- packages/hardhat-zksync-deploy/src/constants.ts
+++ packages/hardhat-zksync-deploy/src/constants.ts
@@ -1,2 +1,3 @@
 export const PLUGIN_NAME = '@matterlabs/hardhat-zksync-deploy';
 export const ETH_DEFAULT_NETWORK_RPC_URL = 'http://localhost:8545';
+export const ETH_DEFAULT_NETWORK_RPC_URL_V2 = 'http://localhost:8545';
